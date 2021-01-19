9e135ad175c22dec732c7a88722c997b4b38fa99
Pho Tran <tranthanhpho2011@gmail.com>
2021-01-19 21:40:14 +0700

    created table

---
+++ app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:app/shared_preferences_table.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
+  sharedPreferences.setBool("isDeveloperMode", true);
+  sharedPreferences.setDouble("scale", 1.0);
+  sharedPreferences.setInt("count", 1);
+  sharedPreferences.setString("name", "@ttpho");
+  sharedPreferences
+      .setStringList("supported_platforms", ["ANDROID", "IOS", "WEB"]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => SharedPreferencesTable()),
+          );
+        },
+        tooltip: 'view',
+        child: Text("Developer View"),
+      ),
+    );
+  }
+}
---
+++ app/lib/shared_preferences_table.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesTable extends StatefulWidget {
+  SharedPreferencesTable({Key key}) : super(key: key);
+
+  @override
+  _SharedPreferencesTableState createState() => _SharedPreferencesTableState();
+}
+
+class Item {
+  final String key;
+  final String value;
+  final String type;
+  const Item({this.key, this.value, this.type});
+}
+
+final headerTexts = ["key", "type", "value"];
+
+class _SharedPreferencesTableState extends State<SharedPreferencesTable> {
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<List<Item>> _futureReadAllPreference() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getKeys().map<Item>((key) {
+      final value = prefs.get(key);
+      return Item(
+        key: key,
+        value: value.toString(),
+        type: value.runtimeType.toString(),
+      );
+    }).toList(growable: false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: CloseButton(),
+        title: Text("Shared Preferences Table"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<Item>>(
+          future: _futureReadAllPreference(),
+          builder: (BuildContext _, AsyncSnapshot<List<Item>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              return DataTableWidget(
+                listItem: snapshot.data,
+              );
+            }
+            return const CircularProgressIndicator();
+          }),
+    );
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List<Item> listItem;
+  DataTableWidget({Key key, @required this.listItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
+                columns: headerTexts
+                    .map<DataColumn>((text) => DataColumn(label: Text(text)))
+                    .toList(growable: false),
+                rows: listItem
+                    .map<DataRow>((Item) => DataRow(
+                          cells: <DataCell>[
+                            DataCell(SelectableText(Item.key)),
+                            DataCell(SelectableText(Item.type)),
+                            DataCell(SelectableText(Item.value)),
+                          ],
+                        ))
+                    .toList(growable: false))));
+  }
+}
